f8500a567ccecf3b1289f8777b5784f7e45d56e4
Emma Ren <emmaren@Emmas-Air.verizon.net>
2021-12-26 13:10:14 -0500

    create tweet retieve comments

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,22 +1,27 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
+from tweets.api.serializers import (
+    TweetSerializer,
+    TweetSerializerForCreate,
+    TweetSerializerForComments,
+)
 from tweets.models import Tweet
 from newsfeeds.services import NewsFeedService
+from utils.decorators import required_params
 
 
 class TweetViewSet(viewsets.GenericViewSet):
     serializer_class = TweetSerializerForCreate
+    queryset = Tweet.objects.all()
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action in ['list', 'retrieve', ]:
             return [AllowAny()]
         return [IsAuthenticated()]
 
+    @required_params(params=['user_id'])
     def list(self, request):
-        if 'user_id' not in request.query_params:
-            return Response('missing user_id', status=400)
         # 实际上返回个字符串，user_id是int，但是支持传进个string类型，django会自动进行类型转换
         user_id = request.query_params['user_id']
         tweets = Tweet.objects.filter(user_id=user_id).order_by('-created_at')
@@ -24,6 +29,12 @@ class TweetViewSet(viewsets.GenericViewSet):
         serializer = TweetSerializer(tweets, many=True)
         # 约定俗成返回一个Jason格式的dict,返回给前端要价格key
         return Response({'tweets': serializer.data})
+
+    def retrieve(self, request, *args, **kwargs):
+        # <HOMEWORK 1> 通过某个 query 参数 with_all_comments 来决定是否需要带上所有 comments
+        # <HOMEWORK 2> 通过某个 query 参数 with_preview_comments 来决定是否需要带上前三条 comments
+        tweet = self.get_object()
+        return Response(TweetSerializerForComments(tweet).data)
 
     def create(self, request):
         serializer = TweetSerializerForCreate(
