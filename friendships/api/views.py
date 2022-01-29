a009f057b88471611f26b9565ce3e364bb9882a8
Emma Ren <emmaren@Emmas-Air.verizon.net>
2022-01-29 16:05:02 -0500

    add tweets paginations

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from utils.paginations import FriendshipPagination
+from friendships.api.paginations import FriendshipPagination
 
 from friendships.api.serializer import (
     FollowerSerializer,
