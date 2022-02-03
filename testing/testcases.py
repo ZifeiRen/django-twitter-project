58b3c0894d2173dba1bc638e30807a54433fb2bc
Emma Ren <emmaren@Emmas-Air.verizon.net>
2022-02-03 15:12:12 -0500

    add limit cache size

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,6 +1,7 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from friendships.models import Friendship
 from rest_framework.test import APIClient
 from utils.redis_client import RedisClient
 from comments.models import Comment
@@ -61,3 +62,6 @@ class TestCase(DjangoTestCase):
 
     def create_newsfeed(self, user, tweet):
         return NewsFeed.objects.create(user=user, tweet=tweet)
+
+    def create_friendship(self, from_user, to_user):
+        return Friendship.objects.create(from_user=from_user, to_user=to_user)
